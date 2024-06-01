19eda2cc31bcc8d468907f11547c7b8158c837eb
fazt <wilson@faztweb.com>
2024-05-31 19:53:25 -0600

    first commit

---
+++ LoginWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace ProyectoFinal2
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Password;
+
+            // Aquí puedes realizar la validación de las credenciales
+            // En este ejemplo, cualquier nombre de usuario y contraseña serán válidos
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
